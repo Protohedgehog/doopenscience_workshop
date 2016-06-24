4b262b7283b736c18ab5d23a3a23051e176ee05c
colombj <julien.colomb@fu-berlin.de>
2016-06-24 17:58:33 +0200

    better bet system and twitter integration

--- R_code/concatenate_bet.r
+++ R_code/concatenate_bet.r
@@ -1,7 +1,8 @@
 # this code will concatenate the survey files to have all data in one table.
 
 # get all filenames in the folder survey
-
+library (dplyr)
+require (twitteR)
 
 Folderdata= ("../Bet_system")
 setwd(Folderdata)
@@ -11,9 +12,35 @@ filenames = dir()
 ## read each file present and concatenate using rbind
 concatenated= c()
 for (i in filenames){
-  N= read.csv(i, header=T)
+  N= read.csv(i, header=T, colClasses = c(rep("character",3), rep( "numeric", 8)))
   concatenated= rbind(concatenated, N)
 }
 
-require (twitteR)
-install.packages("twitteR")
+names(concatenated)
+
+concatenated = concatenated %>%
+    replace(is.na(.), 0) %>%
+    mutate(sum = rowSums(.[c(6,8,10)]) - rowSums(.[c(7,9,11)])) %>%
+    mutate (winner = ifelse(sum>0, "France", "Ireland"))
+
+
+ #   sum (concatenated[,6], concatenated[,8], concatenated[,10])- sum (concatenated[,7], concatenated[,9], concatenated[,11])
+
+#install.packages("twitteR")
+setup_twitter_oauth("tk5IRs1bKELnNYCg5EjthIJce",
+                    "PZvUg8EvErqOyyEElrQKaeWjs1TjkDDQ03tpdHl6pv1Jgh8KY5", access_token=NULL, access_secret=NULL)
+
+
+
+i=2
+
+b= "notontwitter"
+b=getUser(concatenated[i,2])
+Twitteraccount = ifelse( b== "notontwitter", paste(concatenated[i,1]),paste(concatenated[i,2]))
+
+
+text = 
+  paste0( " @",Twitteraccount, " from ", paste(concatenated[i,3]), " bet at #berlinminiwow that ",  concatenated$winner[i], " will win today.")
+  
+
+tweet (text)
